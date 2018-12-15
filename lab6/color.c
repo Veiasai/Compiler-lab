250254cb26680ad40f90ef4b7e44c9d76209b946
Veiasai <917971502@qq.com>
2018-12-15 11:09:18 +0800

    color: coalesce is very very very diffcult

--- lab6/color.c
+++ lab6/color.c
@@ -13,7 +13,7 @@
 #include "color.h"
 #include "table.h"
 
-#define K 14
+#define K 15
 
 static G_nodeList simplifyWorklist;
 static G_nodeList freezeWorklist;
@@ -35,6 +35,8 @@ static Temp_tempList notSpillTemps;
 static G_table degreeTab;
 static G_table colorTab;
 static G_table aliasTab;
+
+static char *hard_regs[17] = {"none", "%rax", "%rbx", "%rcx", "%rdx", "%rsi", "rdi", "rbp", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "rsp"};
 
 struct COL_result COL_color(G_graph ig, Temp_map initial, Temp_tempList regs, Live_moveList moves) {
 	// initial
@@ -73,21 +75,25 @@ struct COL_result COL_color(G_graph ig, Temp_map initial, Temp_tempList regs, Li
 		}
 	}
 
-	AssignColors();
 	struct COL_result ret;
+	Temp_map coloring = Temp_empty();
+	G_nodeList nodes = G_nodes(ig);
+	for (; nodes; nodes = nodes->tail) {
+		int *color = G_look(colorTab, nodes->head);
+		Temp_enter(coloring, Live_gtemp(nodes->head), hard_regs[*color]);
+	}
+
+	ret.coloring = coloring;
+	ret.spills = spilledNodes;
 	return ret;
 }
 
 
 static void Build(G_graph ig) {
 	for (G_nodeList nodes = G_nodes(ig); nodes; nodes = nodes->tail) {
+		/* intial degree */
 		int *degree = checked_malloc(sizeof(int));
-		*degree = 0;
-
-		// i think adj may return duplicate nodes .  ???
-		for (G_nodeList cur = G_succ(nodes->head); cur; cur = cur->tail)
-			(*degree)++;
-		
+		*degree = G_degree(nodes->head);
 		G_enter(degreeTab, nodes->head, degree);
 
 		/* intial color */
@@ -112,15 +118,16 @@ static int locate_register(Temp_temp temp) {
 	else if (temp == F_RSI()) return 5;
 	else if (temp == F_RDI()) return 6;
 	else if (temp == F_RBP()) return 7;
-	else if (temp == F_RSP()) return 8;
-	else if (temp == F_R8()) return 9;
-	else if (temp == F_R9()) return 10;
-	else if (temp == F_R10()) return 11;
-	else if (temp == F_R11()) return 12;
-	else if (temp == F_R12()) return 13;
-	else if (temp == F_R13()) return 14;
-	else if (temp == F_R14()) return 15;
-	else if (temp == F_R15()) return 16;
+	else if (temp == F_R8()) return 8;
+	else if (temp == F_R9()) return 9;
+	else if (temp == F_R10()) return 10;
+	else if (temp == F_R11()) return 11;
+	else if (temp == F_R12()) return 12;
+	else if (temp == F_R13()) return 13;
+	else if (temp == F_R14()) return 14;
+	else if (temp == F_R15()) return 15;
+
+	else if (temp == F_RSP()) return 16;
 
 	return 0;
 }
@@ -135,11 +142,11 @@ static void MakeWorklist(G_graph ig) {
 			continue;
 		}
 
-		if (*degree >= K) { /* spill */
+		if (*degree >= K) {
 			spillWorklist = G_NodeList(nodes->head, spillWorklist);
-		} else if (MoveRelated(nodes->head)) { /* moveRelated */
+		} else if (MoveRelated(nodes->head)) {
 			freezeWorklist = G_NodeList(nodes->head, freezeWorklist);
-		} else { /* simplify */
+		} else {
 			simplifyWorklist = G_NodeList(nodes->head, simplifyWorklist);
 		}
 	}
@@ -149,19 +156,22 @@ static void Simplify() {
 	G_node n = simplifyWorklist->head;
 	simplifyWorklist = simplifyWorklist->tail;
 	selectStack = G_NodeList(n, selectStack);
-	for (G_nodeList nodes = G_adj(n); nodes; nodes = nodes->tail) {
+	for (G_nodeList nodes = Adjacent(n); nodes; nodes = nodes->tail)
 		DecrementDegree(nodes->head);
-	}
+	// check adjacent nodes
 }
 
 static void DecrementDegree(G_node m) {
+	int *color = G_look(colorTab, m);
+	if (*color != 0)
+		return;
+	
 	int *degree = G_look(degreeTab, m);
-	int d = *degree;
 	(*degree)--;
 
-	int *color = G_look(colorTab, m);
-	if (d == K && *color == 0) {
-		EnableMoves(G_NodeList(m, G_adj(m)));
+	// here ==, because nodes whose degree < K has be included in simplifyWorklist.
+	if (*degree == K-1) {
+		EnableMoves(G_NodeList(m, Adjacent(m)));
 		spillWorklist = G_subNodeList(spillWorklist, G_NodeList(m, NULL));
 		if (MoveRelated(m)) {
 			freezeWorklist = G_NodeList(m, freezeWorklist);
@@ -169,4 +179,233 @@ static void DecrementDegree(G_node m) {
 			simplifyWorklist = G_NodeList(m, simplifyWorklist);
 		}
 	}
+}
+
+static void EnableMoves(G_nodeList nodes) {
+	for (; nodes; nodes = nodes->tail) {
+		G_node n = nodes->head;
+		for (Live_moveList m = NodeMoves(n); m; m = m->tail) {
+			if (inMoveList(activeMoves, m->src, m->dst)) {
+				activeMoves = subMoveList(activeMoves, Live_MoveList(m->src, m->dst, NULL));
+				worklistMoves = Live_MoveList(m->src, m->dst, worklistMoves);
+			}
+		}
+	}
+}
+
+
+static void Coalesce() {
+	G_node u, v;
+	G_node x = worklistMoves->src;
+	G_node y = worklistMoves->dst;
+
+	// guarantee at least one is 0 color.   bind v to u.
+	if (precolored(GetAlias(y))) {
+		u = GetAlias(y);
+		v = GetAlias(x);
+	} else {
+		u = GetAlias(x);
+		v = GetAlias(y);
+	}
+	worklistMoves = worklistMoves->tail;
+	if (u == v) {
+		coalescedMoves = Live_MoveList(x, y, coalescedMoves);
+		AddWorkList(u);
+	} else if (precolored(v) || G_inNodeList(u, G_adj(v))) {
+		constrainedMoves = Live_MoveList(x, y, constrainedMoves);
+		// AddWorkList(u);
+		// AddWorkList(v);
+	} else if (Briggs(u, v)) {
+		coalescedMoves = Live_MoveList(x, y, coalescedMoves);
+		Combine(u, v);
+		AddWorkList(u);
+	} else {
+		activeMoves = Live_MoveList(x, y, activeMoves);
+	}
+}
+
+static G_nodeList Adjacent(G_node n) {
+	return G_subNodeList(G_subNodeList(G_adj(n), selectStack), coalescedNodes);
+}
+
+static void AddWorkList(G_node u) {
+	if (!precolored(u) && !MoveRelated(u) && *(int *)G_look(degreeTab, u) < K) {
+		freezeWorklist = G_subNodeList(freezeWorklist, G_NodeList(u, NULL));
+		simplifyWorklist = G_NodeList(u, simplifyWorklist);
+	}
+}
+
+static bool Briggs(G_node u, G_node v) {
+	G_nodeList nodes = G_unionNodeList(Adjacent(u), Adjacent(v));
+	int k = 0;
+	for (; nodes; nodes = nodes->tail) {
+		G_node n = nodes->head;
+		if (*(int *)G_look(degreeTab, n) >= K) {
+			k++;
+		}
+	}
+	return (k < K);
+}
+
+// bind v to u
+static void Combine(G_node u, G_node v) {
+	if (G_inNodeList(v, freezeWorklist)) {
+		freezeWorklist = G_subNodeList(freezeWorklist, G_NodeList(v, NULL));
+	} else {
+		spillWorklist = G_subNodeList(spillWorklist, G_NodeList(v, NULL));
+	}
+	coalescedNodes = G_NodeList(v, coalescedNodes);
+
+	// amazing data structrue...
+	*(G_node *)G_look(aliasTab, v) = u;
+
+	for (G_nodeList t = Adjacent(v); t; t = t->tail) {
+		if (!G_inNodeList(t->head, G_adj(u))) {
+			(*(int *)G_look(degreeTab, u))++;
+			G_addEdge(u, v);
+		}
+		DecrementDegree(t->head);
+	}
+
+	if (*(int *)G_look(degreeTab, u) >= K && G_inNodeList(u, freezeWorklist)) {
+		freezeWorklist = G_subNodeList(freezeWorklist, G_NodeList(u, NULL));
+		spillWorklist = G_NodeList(u, spillWorklist);
+	}
+}
+
+
+static void Freeze() {
+	G_node u = freezeWorklist->head;
+	freezeWorklist = freezeWorklist->tail;
+	simplifyWorklist = G_NodeList(u, simplifyWorklist);
+	FreezeMoves(u);
+}
+
+static void FreezeMoves(G_node u) {
+	for (Live_moveList m = NodeMoves(u); m; m = m->tail) {
+		G_node x = m->src;
+		G_node y = m->dst;
+		G_node v;
+		if (GetAlias(y) == GetAlias(u)) {
+			v = GetAlias(x);
+		} else {
+			v = GetAlias(y);
+		}
+		activeMoves = subMoveList(activeMoves, Live_MoveList(x, y, NULL));
+		frozenMoves = Live_MoveList(x, y, frozenMoves);
+		if (NodeMoves(v) == NULL && *(int *)G_look(degreeTab, v) < K) {
+			freezeWorklist = G_subNodeList(freezeWorklist, G_NodeList(v, NULL));
+			simplifyWorklist = G_NodeList(v, simplifyWorklist);
+		}
+	}
+}
+
+
+static void SelectSpill() {
+	G_node m = NULL;
+	int max = 0;
+	for (G_nodeList p = spillWorklist; p; p = p->tail) {
+		Temp_temp t = G_nodeInfo(p->head);
+		// if (inTempList(notSpillTemps, t)) {
+		// 	continue;
+		// }
+		int degree = *(int *)G_look(degreeTab, p->head);
+		if (degree > max) {
+			max = degree;
+			m = p->head;
+		}
+	}
+
+	if (m) {
+		spillWorklist = G_subNodeList(spillWorklist, G_NodeList(m, NULL));
+		simplifyWorklist = G_NodeList(m, simplifyWorklist);
+		FreezeMoves(m);
+	} else {
+		assert(0);
+	}
+}
+
+static void AssignColors(G_graph ig) {
+	bool okColors[K + 2];
+
+	spilledNodes = NULL;
+	while (selectStack) {
+		okColors[0] = FALSE;
+		for (int i = 1; i < K + 1; i++) {
+			okColors[i] = TRUE;
+		}
+
+		G_node n = selectStack->head;
+		selectStack = selectStack->tail;
+		for (G_nodeList adjs = G_adj(n); adjs; adjs = adjs->tail) {
+			int *color = G_look(colorTab, GetAlias(adjs->head));
+			okColors[*color] = FALSE;
+		}
+
+		int i;
+		bool realSpill = TRUE;
+		for (i = 1; i < K + 1; i++) {
+			if (okColors[i]) {
+				realSpill = FALSE;
+				break;
+			}
+		}
+
+		if (realSpill) {
+			spilledNodes = G_NodeList(n, spilledNodes);
+		} else {
+			*(int *)G_look(colorTab, n) = i;
+		}
+	}
+
+	// color coalesced nodes
+	for (G_nodeList p = G_nodes(ig); p; p = p->tail) {
+		*(int *)G_look(colorTab, p->head) = *(int *)G_look(colorTab, GetAlias(p->head));
+	}
+}
+
+
+static bool MoveRelated(G_node n) {
+	return (NodeMoves(n) != NULL);
+}
+
+// search n in movelist.
+static Live_moveList NodeMoves(G_node n) {
+	Live_moveList moves = NULL;
+	G_node m = GetAlias(n);
+	for (Live_moveList p = unionMoveList(activeMoves, worklistMoves); p; p = p->tail)
+		if (GetAlias(p->src) == m || GetAlias(p->dst) == m) 
+			moves = Live_MoveList(p->src, p->dst, moves);
+	return moves;
+}
+
+// find the original one
+static G_node GetAlias(G_node n) {
+	G_node *res = G_look(aliasTab, n);
+    if (*res != n) return GetAlias(*res);
+    return *res;
+}
+
+static Live_moveList unionMoveList(Live_moveList a, Live_moveList b) {
+    Live_moveList res = a;
+    for (Live_moveList p = b; p; p = p->tail)
+        if (!inMoveList(a, p->src, p->dst)) 
+            res = Live_MoveList(p->src, p->dst, res);
+    return res;
+}
+
+static Live_moveList subMoveList(Live_moveList a, Live_moveList b) {
+    Live_moveList res = NULL;
+    for (Live_moveList p = a; p; p = p->tail) 
+        if (!inMoveList(b, p->src, p->dst)) 
+            res = Live_MoveList(p->src, p->dst, res);
+    return res;
+}
+
+static bool inMoveList(Live_moveList a, G_node src, G_node dst) {
+	for (; a; a = a->tail) 
+		if (a->src == src && a->dst == dst) 
+			return TRUE;
+		
+	return FALSE;
 }
